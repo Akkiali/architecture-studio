5b34a87e8640f56bbb2b64d7b4452a49c9e48c59
Akkiali <akki.akbarali@gmail.com>
2026-08-18 19:06:51 +0530

    update responsivness navbar and UI

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,10 @@
+"use client";
+import {Sheet, SheetContent, SheetTrigger,} from "@/components/ui/sheet";
+import { Button } from "@base-ui/react";
+
+
+
+
 export default function Navbar() {
   return (
     <nav className="flex items-center justify-between border-b border-gray-800 px-6 py-4 md:px-12">
@@ -10,17 +17,33 @@ export default function Navbar() {
       <a href="/" className="text-2xl tracking-widest transition-opacity hover:opacity-50">
       Home
       </a>
-      <a href="/about" className="text-2xl tracking-widest transition-opacity hover:opacity-50">About</a>
-      <a href="/contact" className="text-2xl tracking-widest transition-opacity hover:opacity-50">Contact</a>
+      <a href="/about" className="text-2xl tracking-widest transition-opacity hover:opacity-50">
+      About
+      </a>
+      <a href="/contact" className="text-2xl tracking-widest transition-opacity hover:opacity-50">
+      Contact
+      </a>
+
      </div>
 
        {/* Mobile Navigation */}
-      <button className="text-sm md:hidden">
+    <Sheet>
+      <SheetTrigger
+      render={
+        <button className="text-lg tracking-widest md:hidden"/>
+      }
+      >
         Menu
-      </button>
+      </SheetTrigger>
 
-
-
+      <SheetContent className="bg-[#050505] text-white border-gray-600">
+        <div className="mt-10 flex flex-col gap-8">
+          <a href="/" className="text-2xl transition-opacity hover:opacity-50">Home</a>
+          <a href="/about" className="text-2xl transition-opacity hover:opacity-50">About</a>
+          <a href="/contact" className="text-2xl transition-opacity hover:opacity-50">Contact</a>
+        </div>
+      </SheetContent>       
+      </Sheet>
     </nav>
   );
 }
